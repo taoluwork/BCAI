6926192e933bd41464d943a1f2eb902b9fd5d216
devonmcdonald00 <devonmcdonald00@yahoo.com>
2019-11-04 15:13:50 -0600

    new contract

--- ML/localWorker/workerCLI.js
+++ ML/localWorker/workerCLI.js
@@ -821,6 +821,7 @@ checkEvents = async (showLogs) => {
       if (pastEvents[i].returnValues && hex2ascii(pastEvents[i].returnValues.info) === "Validation Assigned to Provider") {
         if (pastEvents[i] && userAddress === pastEvents[i].returnValues.provAddr) {
             //if (showLogs) console.log("You are a validator", "You need to validate the task for: " + pastEvents[i].reqAddr + " as true or false. The server id is:" + hex2ascii(pastEvents[i].returnValues.extra));
+            console.log("\nIn here this is the request IP " + String(hex2ascii(pastEvents[i].returnValues.extra)) + "\n");
             mode = 1;
             requestAddr = pastEvents[i].returnValues.reqAddr
             requestIP = hex2ascii(pastEvents[i].returnValues.extra)
