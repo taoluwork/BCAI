5c46b1aa5506293ff6b58f375fd947db141d381f
Pedro Rivera <pedrogrivera97@gmail.com>
2019-11-05 13:53:16 -0600

    updates to execute.py

--- ML/localUser/execute.py
+++ ML/localUser/execute.py
@@ -1,53 +1,51 @@
 import os
-import sys
 from flask import Flask
 import requests as r
 import time
-import json
-from signal import signal, SIGINT
+import threading
 
-# 0-> provider
-# 1-> validator
-mode = sys.argv[1]
-ip   = sys.argv[2]
-file = sys.argv[3]
-myIp = sys.argv[4]
-#print('FILE PROCESS BEGINNING')
+def loop():
+    while True:
+        time.sleep(5)
+        ##check file##
+        statF=open("stat.txt", "r")
+        reqIp = statF.readline().rstrip()
+        statF.close()
+        if reqIp != '' and reqIp != 'Executing' and reqIp != 'Ready':
+            statF=open("stat.txt", 'w')
+            statF.close()
+            statF=open("stat.txt", 'w')
+            statF.write("Executing")
+            statF.close()
 
+            res = r.get('http://' + reqIp + '/files')
+            open('image.zip', 'wb').write(res.content)
+            res = r.get('http://' + reqIp + '/exit')
+            os.system("unzip image.zip")
 
-if file == 'none':
-    res = None
-    if len(sys.argv) == 3:
-        ip = sys.argv[1]
-        file = sys.argv[2]
-    if file == 'none' and ip != 'none':
-        #print('requesting files from: ' + ip)
-        res = r.get('http://' + ip + '/files')
-        open('image.zip', 'wb').write(res.content)
-        res = r.get('http://' + ip + '/exit')
-        #unzip file (!!!!assume that image.zip is created by cli!!!!)
-        os.system("unzip image.zip")
+            statF=open("stat.txt", 'w')
+            statF.close()
+            statF=open("stat.txt", 'w')
+            statF.write("Ready")
+            statF.close()
 
-else:
-    app = Flask(__name__)
-    f = open(file , 'rb')
+#########file server###########
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/files')
+def fileRead():
+    if os.path.isfile('image.zip'):
+        f = open('image.zip' , 'rb') 
+        return f.read()
+    else:
+        return b'err'
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=loop)
+    t1.start()
+    app.run(host='0.0.0.0', threaded=False)
     
-    @app.route('/')
-    def hello():
-        return "Hello World!"
-
-    @app.route('/files')
-    def fileRead():
-        file = f.read()
-        #print(type(file))
-        data = file 
-        return data
-    
-    @app.route('/exit')
-    def exit():
-        os.kill(os.getpid(), SIGINT)
-        return "BYE"
-
-    if __name__ == '__main__':
-        app.run(host=myIp)
-        
