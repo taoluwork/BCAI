7bf2fdc9224fdec42463a588a06c5f08a57b7ad6
devonmcdonald00 <devonmcdonald00@yahoo.com>
2019-11-07 13:50:18 -0600

    removed logs

--- ML/localUser/userCLI.js
+++ ML/localUser/userCLI.js
@@ -197,7 +197,7 @@ function receiveResult(){
     fs.readFile('./stat.txt', function read(err, data){
         if (err) throw err;
         fileContent = data;
-        console.log(fileContent.toString('utf8'));
+        //console.log(fileContent.toString('utf8'));
         if(fileContent.toString('utf8') === 'Ready')
         {
             fs.truncate('./stat.txt', 0, function(){
