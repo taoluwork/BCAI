5e3e80cff619623223418b610b1d0c08a8e759f5
SamP98 <samgp918@gmail.com>
2019-11-12 12:53:26 -0600

    Gas Estimation Script Changes

--- Analysis/EstimatePools.js
+++ Analysis/EstimatePools.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3');
-var TaskContract = require('../../../bcai_deploy/client/src/contracts/TaskContract.json');
+var TaskContract = require('../bcai_deploy/client/src/contracts/TaskContract.json');
 var abi = TaskContract.abi;
-var addr = TaskContract.networks[NetworkID].address;        //align to const ID defination on top
+var addr = TaskContract.networks[3].address;        //align to const ID defination on top
 const myContract = new Web3.eth.Contract(abi, addr);
 var account = "0xcc90abef8180d0ab5974dd0f1247623bc246eef8";
 var gas;
@@ -15,14 +15,14 @@ console.log("Begining gas estimate data collection...");
 gas = myContract.methods.getProviderPool.estimateGas({from: account});
 recordGas("GetProviderPool - ");
 
-gas = myContract.methods.getPendingPool.estimateGas({from: account});
-recordGas("GetPendingPool - ");
+// gas = myContract.methods.getPendingPool.estimateGas({from: account});
+// recordGas("GetPendingPool - ");
 
-gas = myContract.methods.getValidatingPool.estimateGas({from: account});
-recordGas("GetValidatingPool - ");
+// gas = myContract.methods.getValidatingPool.estimateGas({from: account});
+// recordGas("GetValidatingPool - ");
 
-gas = myContract.methods.getProvidingPool.estimateGas({from: account});
-recordGas("GetProvidingPool - ");
+// gas = myContract.methods.getProvidingPool.estimateGas({from: account});
+// recordGas("GetProvidingPool - ");
 
 
 console.log("Data collection complete");
