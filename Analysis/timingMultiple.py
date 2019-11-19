8abb55f8007cef79ba415d1d86d80e5d72679ca6
SamP98 <samgp918@gmail.com>
2019-11-19 12:26:00 -0600

    Small timingMultiple updates

--- Analysis/timingMultiple.py
+++ Analysis/timingMultiple.py
@@ -1,8 +1,9 @@
 import subprocess
 
-for i in range(0, 2):
-    process = subprocess.Popen("node TimingAnalysis.js", stdout = subprocess.PIPE)
+for i in range(0, 5):
+    print("Iteration " + str(i) + " is running...\n")
+    process = subprocess.Popen("node TimingAnalysis.js", stdout = subprocess.PIPE, shell=True)
     process.wait()
 
-    for line in process.stdout:
-        print(line)
+    # for line in process.stdout:
+    #     print(line)
