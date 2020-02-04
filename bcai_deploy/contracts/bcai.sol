a61c0945bb67c4134bf5a1c82d9c671620b80d89
SamP98 <samgp918@gmail.com>
2020-02-04 10:21:55 -0600

    Added back line to ArrayPop which may have caused a bug

--- bcai_deploy/contracts/bcai.sol
+++ bcai_deploy/contracts/bcai.sol
@@ -80,7 +80,7 @@ contract TaskContract is bcaiReputation{
     uint256 private providerCount;                       //+1 each time
     uint256 private requestCount;
 
-    constructor() public payable{                               //sol 5.0 syntax
+    constructor() public{                               //sol 5.0 syntax
         providerCount = 0;
         requestCount = 0;
     }
@@ -506,7 +506,7 @@ contract TaskContract is bcaiReputation{
             if (array[i] == target) {
                 array[i] = array[array.length-1];   //swap last element with hole
                 delete array[array.length-1];       //delete last item
-                //array.length -= 1;                  //decrease size
+                array.length -= 1;                  //decrease size
                 return true;
             }
         }
