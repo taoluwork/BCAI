4bb1a01d7241c01ea80cb45d5cd6ca3b8db3aefd
Pedro Rivera <pedrogrivera97@gmail.com>
2020-02-13 13:39:11 -0600

    cleaning up the hosting code

--- Testing/onionPar/host.py
+++ Testing/onionPar/host.py
@@ -8,9 +8,11 @@ from signal import signal, SIGINT
 import threading
 from datetime import datetime
 
-threads = 30
+threads = 4
 threadL = []
 orderAddr = []
+order   = []
+startTimes = []
 def shareOrder():
     os.system("script -c \"~/onionshare/dev_scripts/onionshare --website totalOrder.txt" + "\" -f onionshareOrder.txt")
 def startShare(file, iter):
@@ -23,7 +25,7 @@ def splitFile(file):
     lines   = f.readlines()
     lineLen = len(lines)
     pos     = 0
-    order   = []
+
     print(lines)
     print(lineLen)
     for i in range(0, threads):
@@ -46,10 +48,10 @@ def cleanFiles():
 
 def createThreads():
     f = open("order.txt" , 'r')
-    order = f.readlines()
+    orderFile = f.readlines()
     f.close()
     j = 0
-    for i in order:
+    for i in orderFile:
         t=threading.Thread(target=startShare,args=[i.strip('\n'),j]) 
         threadL.append(t)
         j += 1
@@ -57,10 +59,17 @@ def createThreads():
 def runThreads():
     for i in threadL:
         i.start()
+        startTimes.append(time.time())
 
 def getAddrs():
-    while len(orderAddr) != threads:
-        print(len(orderAddr))
+    for i in range(0,threads):
+        orderAddr.append(0)
+    t = 0
+    while t < threads:
+        t = 0
+        for i in orderAddr:
+            if i != 0:
+                t +=1
         for i in range(0,threads):
             if os.path.isfile('onionshare'+str(i)+'.txt'):
                 f = open('onionshare'+str(i)+'.txt', 'r')
@@ -68,7 +77,8 @@ def getAddrs():
                 f.close()
                 for j in lines:
                     if (j.find("http://onionshare") >= 0): #found address
-                        orderAddr.append(j.strip('/n'))
+                        orderAddr[i] = j.strip('\n') + "/" + order[i].strip('\n')
+        print(orderAddr)    
         time.sleep(5)
     print(orderAddr)
     f = open('totalOrder.txt', 'w')
@@ -76,10 +86,31 @@ def getAddrs():
         f.write(i + '\n')
     f.close()
 
+def threadRestarter():
+    while(True):
+        for i in range(0,threads):
+            if time.time() > startTimes[i] + 120 and orderAddr[i] == 0:
+                os.system('rm onionshare' + str(i) + '.txt')
+                threadL[i]._delete()
+                f = open("order.txt" , 'r')
+                lines = f.readlines()
+                f.close()
+                t=threading.Thread(target=startShare,args=[lines[i].strip('\n'),i]) 
+                threadL[i] = t
+                threadL[i].start()
+                startTimes[i] = time.time()
+                f = open('restart.txt', 'a')
+                f.write("thread:" + str(i) + ' has been restarted at:' + str(time.time()) + '\n')
+                f.close()
+        time.sleep(5)
+
+
 cleanFiles()
-# splitFile("hello.txt")
-# createThreads()
-# runThreads()
-# getAddrs()
-# bigThread = threading.Thread(target=shareOrder)
-# bigThread.start()
+splitFile("hello.txt")
+createThreads()
+runThreads()
+watch = threading.Thread(target=threadRestarter)
+watch.start()
+getAddrs()
+bigThread = threading.Thread(target=shareOrder)
+bigThread.start()
