6ccce801afaea94f5be78b903a1aa24431333517
cjohnson57 <c.johnson.57@outlook.com>
2020-03-03 19:00:44 -0600

    Fixed a couple issues in execute.py, need to figure out why running out of memory

--- Testing/onionPar/execute.py
+++ Testing/onionPar/execute.py
@@ -118,7 +118,7 @@ def getTotalAddr():
 def threadRestarter():
     while(True):
         for i in range(0,threads):
-            if time.time() > startTimes[i] + 120 and orderAddr[i] == 0:
+            if time.time() > startTimes[i] + 60 and orderAddr[i] == 0:
                 os.system('rm onionshare' + str(i) + '.txt')
                 threadL[i]._delete()
                 f = open("order.txt" , 'r')
@@ -194,7 +194,7 @@ def totalThreadRestarter():
     global totalAddr
     global mainThread
     while (True):
-        if time.time() > totalStartTime + 120 and totalAddr == 0:
+        if time.time() > totalStartTime + 60 and totalAddr == 0:
             os.system('rm onionshareOrder.txt')
             #restart thread
             mainThread._delete()
@@ -322,17 +322,19 @@ def createThreadsReq():
             content2 = []
             for i in range (0, threads):
                 for j in range(0, len(content[i])):
-                    content2.append(content[i][j])
+                    content2.append(bytes(content[i][j]))
             #And now it can be saved with a join
-            #open("image.zip", "wb").write(b''.join(content2))
-            fileWrite = open('image.zip', 'w')
-            fileWrite.close()
-            fileWrite = open('image.zip', 'ab')
-            stepsize = 1000000
-            for i in range(0, math.floor(len(content2)/stepsize)-1 ) :
-                fileWrite.write(b''.join(content2[i:i+stepsize]))
-            fileWrite.write(b''.join(content2[(len(content2)/stepsize)+stepsize : len(content2)]))
-            fileWrite.close()
+            open("image.zip", "wb").write(b''.join(content2))   
+            
+            # fileWrite = open('image.zip', 'w')
+            # fileWrite.close()
+            # fileWrite = open('image.zip', 'ab')
+            # stepsize = 1000000
+            # for i in range (0,  math.floor(len(content2)/stepsize)):
+            #     fileWrite.write(b''.join(content2[i*stepsize:(i+1)*stepsize]))
+            # fileWrite.write(b''.join(content2[ math.floor(len(content2)/stepsize)*stepsize:len(content2)]))
+            # fileWrite.close()   
+
             resetReq()
             flag = False
         #totalOrder.txt not yet received (Step 1)
