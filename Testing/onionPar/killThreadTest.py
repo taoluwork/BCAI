d4c32599c854ab3781dd61351b779f6fdc400931
Pedro Rivera <pedrogrivera97@gmail.com>
2020-03-17 21:57:16 -0500

    fixing the thread killing test

--- Testing/onionPar/killThreadTest.py
+++ Testing/onionPar/killThreadTest.py
@@ -4,28 +4,38 @@ import time
 import threading
 from signal import signal, SIGINT
 import subprocess
+import multiprocessing
 
 threads = 10
 proc = []
+threadL=[]
 def shareOrder(iter):
     global proc
-    proc.append(0)
-    proc[iter] = subprocess.Popen(["script -c \"~/onionshare/dev_scripts/onionshare --website hello.txt\" -f helloLog" + str(iter) + ".txt"], shell=True)
-
+    procT = subprocess.Popen(["script -c \"~/onionshare/dev_scripts/onionshare --website hello.txt\" -f helloLog" + str(iter) + ".txt"], shell=True)
+    proc.append(procT)
 #create thread
 for i in range(0,threads):
-    thread = threading.Thread(target=shareOrder,args=[i])
+    thread = multiprocessing.Process(target=shareOrder,args=(i,))
     #start thread
     thread.start()
+    threadL.append(thread)
 
 time.sleep(120)
 
+f = open("output.txt", 'w')
+#for i in range(0,threads):
+#    try:
+#        proc[i].terminate()
+#        f.write("proc terminate pass:" + str(i) + "\n")
+#    except:
+#        f.write("proc terminate failure:" + str(i) + "\n")
+
 for i in range(0,threads):
     try:
-        proc[i].terminate()
-        print("pass:" + str(i) + "\n")
+        threadL[i].terminate()
+        f.write("thread terminate pass:" + str(i) + "\n")
     except:
-        print("failure:" + str(i) + "\n")
-
+        f.write("thread terminate failure:" + str(i) + "\n")
 
 print("I am still working")
+f.close()
