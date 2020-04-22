55553c69aa555b07dc1aa5b64c69442d478625fe
cjohnson57 <c.johnson.57@outlook.com>
2020-04-22 17:01:13 -0500

    Created new version of executeDemo from recent changes to execute

--- ML/localWorker/executeDemo.py
+++ ML/localWorker/executeDemo.py
@@ -20,7 +20,11 @@ from datetime import datetime
 import math
 import subprocess
 import multiprocessing
+from multiprocessing import Manager, Value
+from ctypes import c_char_p
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 ##globals##
 threads = 8
@@ -29,23 +33,78 @@ orderAddr = []
 order   = []
 startTimes = []
 mainThread = None
-totalAddr = ''
-totalStartTime = 0
+manager = Manager()
+totalAddr = manager.Value(c_char_p, '')
+totalStartTime = Value('d', 0.0)
 content = []
 for i in range(threads):
     content.append(b'')#inits list with threads number of empty byte arrays
 mode = '' #user, provider, or validator
 fileName = ''
 
+encKey     = None
+encNonce   = None
+#######################################################################################################################################
+#######################################################encryption######################################################################
+#######################################################################################################################################
+
+def genKey():
+    keyFile = "key.txt"
+    nonceFile = "nonce.txt"
+    f = open(keyFile, 'w')
+    f.close()
+    f = open(nonceFile, 'w')
+    f.close()
+    key = os.urandom(32)
+    nonce = os.urandom(32)
+    f = open(keyFile, 'wb')
+    f.write(key)
+    f.close()
+    f = open(nonceFile, 'wb')
+    f.write(nonce)
+    f.close()    
+def getKey(keyFile="", nonceFile=""):
+    f = open(keyFile, 'rb')
+    key = f.read()
+    f.close()
+    f = open(nonceFile, 'rb')
+    nonce = f.read()
+    f.close()
+    return [key, nonce]
+
+def enc(key=b"", nonce=b"", mess=b""):
+    alg = algorithms.AES(key)
+    cipher = Cipher(alg, modes.GCM(nonce), default_backend())
+    encryptor = cipher.encryptor()
+    return encryptor.update(mess)
+
+def dec(key=b"", nonce=b"", mess=b""):
+    alg = algorithms.AES(key)
+    cipher = Cipher(alg, modes.GCM(nonce), default_backend())
+    decryptor = cipher.decryptor()
+    return decryptor.update(mess)
+
+genKey()
+
+
 #######################################################################################################################################
 ###########################################################host########################################################################
 #######################################################################################################################################
 def shareOrder():
     global totalStartTime
-    while os.path.isfile('totalOrder.txt') != True:
+    while os.path.isfile('totalOrder.txt') != True:        
         time.sleep(5)
-    totalStartTime = time.time()
-    subprocess.Popen(["script -c \"../../../onionshare/dev_scripts/onionshare --website totalOrder.txt" + "\" -f onionshareOrder.txt"],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL,shell=True)
+    totalStartTime.value = time.time()
+
+    ######zip the total order, key, and nonce to share#########
+
+    os.system('zip totalOrder.zip totalOrder.txt key.txt nonce.txt  >/dev/null 2>&1')
+    time.sleep(5)    
+
+    ###########################################################
+
+    subprocess.Popen(["script -c \"../../../onionshare/dev_scripts/onionshare --website totalOrder.zip" + "\" -f onionshareOrder.txt"],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL,shell=True)
+
 def startShare(file, iter):
     #print(file + ":" + str(iter))
     #start onionshare server to host file
@@ -68,6 +127,20 @@ def splitFile(file):
         fw.writelines(lines[lo:hi])
         fw.close()
         order.append(file+str(i)+'.txt\n') 
+        keyFile = open("key.txt", 'rb')
+        key = keyFile.read()
+        keyFile.close()
+        nonceFile = open("nonce.txt", "rb")
+        nonce = nonceFile.read()
+        nonceFile.close()
+        fenc = open(file+str(i)+'.txt', "rb")
+        hold = enc(key, nonce, fenc.read())
+        fenc.close()
+        fenc = open(file + str(i) + ".txt", "w")
+        fenc.close
+        fenc = open(file+str(i)+".txt", "wb")
+        fenc.write(hold)
+        fenc.close()
     f.close()
     f = open('order.txt', 'w')
     f.writelines(order)
@@ -126,12 +199,12 @@ def getTotalAddr():
             f.close()
             for j in lines:
                 if (j.find("http://onionshare") >= 0): #found address
-                    totalAddr = j.strip('\n') + "/totalOrder.txt"
+                    totalAddr.value = j.strip('\n') + "/totalOrder.zip"
                     flag = False 
         time.sleep(5)
     #Write address to file
     f = open('totalOrderAddress.txt', 'w')
-    f.write(totalAddr)
+    f.write(totalAddr.value)
     f.close()
 
 def threadRestarter():
@@ -259,13 +332,18 @@ def reqFail():
             failThread.terminate()
             threadOn = False
 
+#################################################################################################################
+#################################################################################################################
+#################################################################################################################
 def totalThreadRestarter():
     global totalStartTime
     global totalAddr
     global mainThread
+    mainThread = multiprocessing.Process(target=shareOrder)
+    mainThread.start()
     while (True):
-        if totalStartTime != 0 and time.time() > totalStartTime + 60 and totalAddr == '':
-            os.system('rm onionshareOrder.txt')
+        if (totalStartTime.value != 0.0) and time.time() > (totalStartTime.value + 60) and totalAddr.value == '':
+            os.system('rm onionshareOrder.txt  >/dev/null 2>&1')
             #restart thread
             #mainThread._delete()
             mainThread.terminate()
@@ -273,16 +351,18 @@ def totalThreadRestarter():
             t = multiprocessing.Process(target=shareOrder)
             mainThread = t
             mainThread.start()
-            totalStartTime = time.time()
             f = open('restart.txt', 'a')
             f.write("thread: for totalOrder has been restarted at:" + str(time.time()) + ' due to time issue\n')
             f.close()
+
+        time.sleep(5)
 
 def resetHost(resetMode):
     global threadL
     global orderAddr
     global order
     global startTimes
+    global totalStartTime
     global mode
     global fileName
     global totalAddr
@@ -295,9 +375,10 @@ def resetHost(resetMode):
     orderAddr = []
     order   = []
     startTimes = []
+    totalStartTime.value = 0.0
     if resetMode == True:
         mode = ''
-    totalAddr = ''
+    totalAddr.value = ''
     try:
         os.system('rm restart.txt totalOrderAddress.txt totalOrder.txt onionShareOrder.txt onionshare*.txt order.txt image.zip*.txt >/dev/null 2>&1')
     except:
@@ -334,7 +415,7 @@ def failingCheck():
             session.proxies['http'] = 'socks5h://localhost:9050'
             session.proxies['https'] = 'socks5h://localhost:9050'
 
-            fails = session.get(totalAddr + '/reqFails.txt')
+            fails = session.get(totalAddr.value + '/reqFails.txt')
             f = open('reqFails.txt', 'wb').write(fails.contetn)
             f.close()
             f = open('reqFails.txt', 'r')
@@ -369,8 +450,15 @@ def getShare(address, iter):
 
     res = session.get(address) #download file
     #content[iter] = res.content #append this slice's content to total content list
+
+
+    ########################get key and nonce##################################
+    [key, nonce] = getKey("key.txt","nonce.txt")
+    ###########################################################################
+    
+    
     f = open("image.zip" + str(iter) + ".txt","wb" )
-    f.write(res.content)
+    f.write(dec(key, nonce, res.content))
     f.close()
     #print(type("-----Received content from thread " + iter))
     #for i in range(threads):
@@ -385,7 +473,12 @@ def getShareWithoutIter(address):
     session.proxies['https'] = 'socks5h://localhost:9050'
 
     res = session.get(address) #download file
-    open("totalOrder.txt", 'wb').write(res.content)
+
+    #########save the zip and unzip it#########
+    open("totalOrder.zip", 'wb').write(res.content)
+    time.sleep(5)
+    os.system("unzip -o totalOrder.zip")
+    ###########################################
     
 def createThreadsReq():
     global totalAddr
@@ -394,7 +487,7 @@ def createThreadsReq():
     flag = True
     flagTwo = True
     flagThree = True
-    #Most things here removed since they're just various steps in waiting for the file to be done transferring
+      #Most things here removed since they're just various steps in waiting for the file to be done transferring
     while flag:
         time.sleep(5)
         #Addresses written to file (Step 2)
@@ -412,46 +505,21 @@ def createThreadsReq():
         #         threadL.append(t) 
         #         t.start()
         #         j += 1
-        #Every slot in content has been written to (Step 3)
-        # elif not (b'' in content):
-        #     #print(content)
-        #     #Tell session it has finished
-
-        #     session = r.session()
-        #     session.proxies = {}
-        #     session.proxies['http'] = 'socks5h://localhost:9050'
-        #     session.proxies['https'] = 'socks5h://localhost:9050'
-
-        #     session.get(totalAddr + '/finish') #tell server finished downloading
-
-        #     try:
-        #         print(type(content))
-        #         print(type(content[0]))
-        #         print(type(content[0][0]))
-        #     except:
-        #         print("hello")
-
-        #     #Save in chunks, converting to bytes
-        #     with open("image.zip", "wb") as f:
-        #         for i in range(threads):
-        #                 f.write(content[i])  
-
-        #     resetReq()
-        #     flag = False
-        #Every slot in content has been written to (Step 3)
+        # #Every slot in content has been written to (Step 3)
         # allVal = True
         # for i in range(0,threads):
         #     if os.path.isfile("image.zip" + str(i) + ".txt"):
         #         content[i] = True
         #     else:
         #         allVal = False
+        #         break
         # if allVal:
         #     if mode == 'user' or mode == 'provider':
         #         session = r.session()
         #         session.proxies = {}
         #         session.proxies['http'] = 'socks5h://localhost:9050'
         #         session.proxies['https'] = 'socks5h://localhost:9050'
-        #         session.get(totalAddr + '/finish') #tell server finished downloading
+        #         session.get(totalAddr.value + '/finish') #tell server finished downloading
         #     totalFile = open('image.zip', 'wb')
         #     for i in range(0, threads):
         #         iterFile = open('image.zip' + str(i) + '.txt', 'rb')
@@ -464,10 +532,10 @@ def createThreadsReq():
         #totalOrder.txt not yet received (Step 1)
         if flagThree: 
             statF = open("stat.txt", 'r')
-            totalAddr = statF.readline().rstrip()
+            totalAddr.value = statF.readline().rstrip()
             statF.close()
             #if file ready to be received from worker. totalAddr will hold the .onion address
-            if totalAddr != '' and totalAddr != 'Executing' and totalAddr != 'Ready':
+            if totalAddr.value != '' and totalAddr.value != 'Executing' and totalAddr.value != 'Ready':
                 flagThree = False
                 #getShareWithoutIter(totalAddr) #Do not start transfer
                 flag = False #This will cause loop to exit and image.zip to start running
@@ -478,7 +546,6 @@ def createThreadsReq():
                     session.proxies['http'] = 'socks5h://localhost:9050'
                     session.proxies['https'] = 'socks5h://localhost:9050'
                     session.get(totalAddr + '/finish') #tell server finished downloading
-
 
 def resetReq():
     global content
@@ -498,7 +565,7 @@ def resetReq():
         except: pass
     threadL = []
     mainThread = None
-    totalAddr = ''
+    totalAddr.value = ''
     mode = ''
     try:
         os.system('rm totalOrder.txt onionShareOrder.txt image.zip*.txt')
@@ -545,6 +612,7 @@ def getTime(mess):
     f.close()
 
 def hostController(file):
+    genKey()
     for i in range(0,threads):
         orderAddr.append(0)
     splitFile(file)
@@ -556,11 +624,7 @@ def hostController(file):
     #failThread = threading.Thread(target=reqFail)
     failThread = multiprocessing.Process(target=reqFail)
     failThread.start()
-    #Total share
     global mainThread
-    #mainThread = threading.Thread(target=shareOrder)
-    mainThread = multiprocessing.Process(target=shareOrder)
-    mainThread.start()
     #Restarter for total share
     #errCorrMain = threading.Thread(target=totalThreadRestarter)
     errCorrMain = multiprocessing.Process(target=totalThreadRestarter)
@@ -575,17 +639,17 @@ def hostController(file):
                 if "/finish" in line :
                     flag = False
                     try: #May or may not already be deleted
-                        #errCorr._delete()
                         errCorr.terminate()
                     except: pass
                     try: #May or may not already be deleted
-                        #mainThread._delete()
+                        errCorrMain.terminate()
+                    except: pass
+                    try: #May or may not already be deleted
                         mainThread.terminate()
                     except: pass      
                 line = f.readline()
             f.close()
     try: #May or may not already be deleted
-        #failThread._delete()
         failThread.terminate()
     except: pass
     resetHost(True)
@@ -603,6 +667,13 @@ def reqController():
 def dockerExe():
     global mode
     time.sleep(30)
+
+    f = open('stat.txt', 'w')
+    f.close()
+    f = open('stat.txt', 'w')
+    f.write('Executing')
+    f.close()
+
     #this will load the image back into docker
     os.system("unzip image.zip")
     os.system("sudo docker load -i image.tgz")
@@ -633,6 +704,7 @@ def dockerExe():
     os.system("sudo docker stop $(sudo docker container ls -q)")
 
     getTime('Image Unloaded and Ready For Transmission')
+    time.sleep(30)
 
 def getMode():
     global mode
@@ -658,6 +730,8 @@ def submitTask():
 
 
 if __name__ == '__main__':
+    f = open('mode.txt', 'w') #make sure mode is cleared on startup
+    f.close()
     while True:
         getMode()    
         if mode == 'user':
@@ -676,9 +750,17 @@ if __name__ == '__main__':
             reqController()
             dockerExe()
             submitTask()
+            try:
+                os.system("rm -rf totalOrder.txt totalOrder.zip")
+            except:
+                pass
             hostController('image.zip')
         elif mode == 'validator':
             resetHost(False)
             reqController()
             dockerExe()
+            try:
+                os.system("rm -rf totalOrder.txt totalOrder.zip")
+            except:
+                pass
             submitTask()
