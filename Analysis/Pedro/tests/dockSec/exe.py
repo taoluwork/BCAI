0c3011c6b8cd5bf9396d95c59c81337eee4711e3
Taurus <taoluwork@gmail.com>
2020-08-21 20:39:41 -0700

    remove all files and replace the latest version

--- Analysis/Pedro/tests/dockSec/exe.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-os.system("sudo docker run -dit execute:latest bash")
-os.system("sudo docker exec $(sudo docker container ls -q) python3 web.py" )
-os.system('sudo docker cp $(sudo docker container ls -q):netTest.csv .')
-os.system('sudo mv netTest.csv netTest' + sys.argv[1] + '.csv')
-os.system("sudo docker stop $(sudo docker container ls -q)")
--- startUser.bash
+++
@@ -1,3 +0,0 @@
-cd ML/localUser
-gnome-terminal --title="BCAI User Console" --tab -x node userCLI.js 
-gnome-terminal --title="BCAI User File Hosting/Transfer" --tab -x sudo python3 execute.py
